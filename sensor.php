61dae35123d07fff0fef123b2d3793ab8ce2b02d
Zulfaqar <zul.kreydle@gmail.com>
2016-02-26 22:16:16 +0800

    time stamping test

--- sensor.php
+++ sensor.php
@@ -2,8 +2,8 @@
  //This file stores the data posted from the CC3000 in your MySQL database
 function db_connect()
 {
-   //$result = mysql_connect("exato-db-instance.cwbw53vhehej.us-west-2.rds.amazonaws.com", "Zulfaqar","94025467z"); 
-    $result = mysql_connect("localhost","root","");
+   $result = mysql_connect("exato-db-instance.cwbw53vhehej.us-west-2.rds.amazonaws.com", "Zulfaqar","94025467z"); 
+    //$result = mysql_connect("localhost","root","");
    if (!$result)
       return false;
    return $result;
@@ -15,8 +15,8 @@ mysql_select_db("exato_database");
     if ($_GET["temp"] && $_GET["hum"]) {
     $temp = $_GET["temp"];
     $hum  = $_GET["hum"];
-$sqlt = "insert into readings (sensor, time_stamp, value) values ('pool_temp',unix_timestamp(),$temp)";
-$sqlh = "insert into readings (sensor, time_stamp, value) values ('pool_hum',unix_timestamp(),$hum)";
+$sqlt = "insert into readings (sensor,value) values ('pool_temp',$temp)";
+$sqlh = "insert into readings (sensor,value) values ('pool_hum',$hum)";
 mysql_query($sqlt);
 mysql_query($sqlh);
     }
